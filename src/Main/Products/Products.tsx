485936e0e67d1719ce026a9376078c78ecd91381
Oleksii <pssxopatik673@gmail.com>
2024-06-19 16:18:01 +0200

    h1 h3 adaptive

--- src/Main/Products/Products.tsx
+++ src/Main/Products/Products.tsx
@@ -196,7 +196,7 @@ const Products = (props: ProductsPropsType) => {
 
     return (
         <div className={`${s.productsSection} ${'mt-10'}`}>
-            <h3 className="mb-6 text-gray-900 dark:text-amber-50">{props.language === 'ua' ? 'Товари' : props.language === 'pl' ? 'Towary' : 'Products'}</h3>
+            <h3 className="mb-6 text-gray-900 dark:text-amber-50 text-lg md:text-2xl xl:text-3xl">{props.language === 'ua' ? 'Товари' : props.language === 'pl' ? 'Towary' : 'Products'}</h3>
             <form className="max-w-screen-sm mx-auto" onSubmit={handleSearchSubmit}>
                 <label htmlFor="default-search"
                        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
@@ -214,7 +214,7 @@ const Products = (props: ProductsPropsType) => {
                            className="block w-[79%] h-8 visible p-[17px] ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                            placeholder={props.language === 'ua' ? "Впиши номер відео тут (нп. V27)"
                                : props.language === 'pl' ? "Wpisz tutaj numer video (np. V27)"
-                                   : "Type video number here (for ex. V27)"} required/>
+                                   : "Type video number here (for ex. V27)"}/>
                     <button type="submit"
                             className="text-white absolute end-2.5 bottom-0 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{props.language === 'ua' ? "Шукати"
                         : props.language === 'pl' ? "Szukaj"
