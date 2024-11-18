aefcdd0f823d329741666ebb722a740e4e215bec
Oleksii <pssxopatik673@gmail.com>
2024-11-18 08:10:28 +0100

    was changed some style in SubCategories component

--- src/Main/Categories/SubCategories.tsx
+++ src/Main/Categories/SubCategories.tsx
@@ -51,34 +51,34 @@ const SubCategories = (props: SubCategoryPropsType) => {
 
     return (
         <div>
-            <h2 className="text-gray-900 dark:text-white">{props.language === 'ua' ? props.CategoryTitle.ua : props.language === 'pl' ? props.CategoryTitle.pl : props.CategoryTitle.en}</h2>
+            <h2 className="mt-4 mb-8 md:text-lg text-gray-900 dark:text-white">{props.language === 'ua' ? props.CategoryTitle.ua : props.language === 'pl' ? props.CategoryTitle.pl : props.CategoryTitle.en}</h2>
 
             <div id="default-carousel" className="relative w-full flex justify-center" data-carousel="slide">
-                <div className="relative h-auto overflow-hidden rounded-lg w-[93%] md:w-[80%] lg:w-[85%] 2xl:w-[88%]">
+                <div className="relative h-auto md:h-[200px] xl:h-[300px] 2xl:h-[350px] 3xl:h-[370px] 4xl:h-[450px] overflow-hidden rounded-lg w-[93%] md:w-[80%] lg:w-[85%] 2xl:w-[88%]">
                     {props.Subcategories?.map((SubCategory, index) => (
                         <a href={SubCategory.link} key={index} target="_blank"
                            className={`h-[100%] target:_blank transition-all duration-700 ease-in-out flex flex-col md:flex-row items-center
                             bg-white border border-gray-200 rounded-lg shadow
                              hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700
                               ${index === currentIndex ? 'block' : 'hidden'}`} data-carousel-item>
-                            {SubCategory.img ?
-                                <img
-                                    className="object-cover w-full rounded-t-lg h-44 md:h-auto md:w-[35%] md:rounded-none md:rounded-s-lg"
-                                    src={SubCategory.img} alt={SubCategory.link}/>
-                                :
-                                <img
-                                    className="object-cover w-full rounded-t-lg h-44 md:h-auto md:w-[35%] md:rounded-none md:rounded-s-lg"
-                                    src={errorImg} alt={'error'}/>
-                            // здобити онлоадер
-                            }
+                                {SubCategory.img ?
+                                    <img
+                                        className="object-cover w-full rounded-t-lg h-44 md:h-[100%] md:w-[35%] md:rounded-none md:rounded-s-lg"
+                                        src={SubCategory.img} alt={SubCategory.title.en}/>
+                                    :
+                                    <img
+                                        className="object-cover w-full rounded-t-lg h-44 md:h-auto md:w-[35%] md:rounded-none md:rounded-s-lg"
+                                        src={errorImg} alt={'error'}/>
+                                    // здобити онлоадер
+                                }
 
 
                             <div
-                                className="flex flex-col justify-between p-4 leading-normal h-[40%] md:h-[100%] w-[90%]">
-                                <h3 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white 3xl:text-4xl 2xl:text-2xl text-sm">{props.language === 'ua' ? SubCategory.title.ua : props.language === 'pl' ? SubCategory.title.pl : SubCategory.title.en}</h3>
-                                <p className="mb-2 font-normal text-gray-700 dark:text-gray-400 3xl:text-2xl 2xl:text-lg text-sm">{props.language === 'ua' ? SubCategory.description.ua : props.language === 'pl' ? SubCategory.description.pl : SubCategory.description.en}</p>
+                                className="flex flex-col justify-between p-4 leading-normal h-[40%] md:h-[100%] w-[90%] md:w-[65%]">
+                                <h3 className="mb-2 mt-6 md:mt-0 font-bold tracking-tight text-gray-900 dark:text-white 3xl:text-2xl 2xl:text-xl text-sm">{props.language === 'ua' ? SubCategory.title.ua : props.language === 'pl' ? SubCategory.title.pl : SubCategory.title.en}</h3>
+                                <p className="mb-2 font-normal md:text-sm text-gray-700 dark:text-gray-400 3xl:text-xl 2xl:text-lg text-sm">{props.language === 'ua' ? SubCategory.description.ua : props.language === 'pl' ? SubCategory.description.pl : SubCategory.description.en}</p>
                                 <button type="button"
-                                        className="flex justify-center mt-4 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                                        className="flex justify-center mb-10 mt-4 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">
                                     <p className="3xl:text-lg text-[12px]">{props.language === 'ua' ? SubCategory.title.ua : props.language === 'pl' ? SubCategory.title.pl : SubCategory.title.en}{' '}
                                     </p>
                                     <svg className="w-6 h-6 text-white" aria-hidden="true"
